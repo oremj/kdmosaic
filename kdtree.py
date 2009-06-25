e16fd11bba3cd6f0d18197d3adeaf1e393d133ef
Jeremy Orem <jeremy.orem@gmail.com>
2009-06-25 10:53:45 -0700

    first commit

---
+++ kdtree.py
@@ -0,0 +1,86 @@
+class Node:pass
+class P:
+    def __init__(self, data, point):
+        self.data = data
+        self.point = tuple(int(p) for p in point)
+ 
+def hp_closest(point, hp):
+    p = []
+    for d in range(len(point)):
+        hr_min = hp[0][d]
+        hr_max = hp[1][d]
+        t = point[d]
+        if t <= hr_min: p.append(hr_min)
+        elif hr_min < t < hr_max: p.append(t)
+        else: p.append(hr_max)
+    return tuple(p) 
+        
+def kdtree(pointList, depth=0):
+    if not pointList:
+        return
+ 
+    # Select axis based on depth so that axis cycles through all valid values
+    k = len(pointList[0].point) # assumes all points have the same dimension
+    axis = depth % k
+ 
+    # Sort point list and choose median as pivot element
+    pointList.sort(key=lambda point: point.point[axis])
+    median = len(pointList)/2 # choose median
+ 
+    # Create node and construct subtrees
+    node = Node()
+    node.axis = axis
+    node.location = pointList[median]
+    node.leftChild = kdtree(pointList[0:median], depth+1)
+    node.rightChild = kdtree(pointList[median+1:], depth+1)
+    return node
+
+
+dist_cache = {}
+def distance(p1, p2):
+    global dist_cache
+    try:
+        return dist_cache[p1,p2]
+    except KeyError:
+        return sum((i - j) ** 2 for i,j in zip(p1, p2))
+
+def nearestn(point,root,best=(None, 1e400), hr=None):
+    if hr == None:
+        hr = []
+        hr.append([0 for i in range(len(point))])
+        hr.append([float('inf') for i in range(len(point))])
+    try: 
+        n = root
+        axis = n.axis
+    except:
+        return None, float('inf')
+    
+    # split rectangle
+    left_hr = hr[:]
+    left_hr[1][axis] = n.location.point[axis]
+    right_hr = hr[:]
+    right_hr[0][axis] = n.location.point[axis]
+
+    if point[axis] <= n.location.point[axis]: 
+        nearer_kd = n.leftChild
+        further_kd = n.rightChild
+        nearer_hr = left_hr
+        further_hr = right_hr
+    else:
+        nearer_kd = n.rightChild
+        further_kd = n.leftChild
+        nearer_hr = right_hr
+        further_hr = left_hr
+    nearest, dist = nearestn(point, nearer_kd, best, nearer_hr)
+
+    if dist < best: best = dist
+    
+    if distance(point, hp_closest(point, further_hr)) < best:
+        if distance(point, n.location.point) < best:
+            nearest = n
+            best = distance(point, n.location.point)
+        tmp_nearest, tmp_dist = nearestn(point, further_kd, best, further_hr)
+        if tmp_dist < best:
+            nearest, best = tmp_nearest, tmp_dist
+
+    return nearest, best
---
+++ mosaic.py
@@ -0,0 +1,83 @@
+import Image
+from collections import defaultdict
+import sys
+import kdtree
+import os
+import os.path
+
+
+class ImageOps:
+    favicon_path = os.path.join(os.path.dirname(__file__),'favicons_sample')
+    def __init__(self, image):
+        self.image_name = image
+        self.im = Image.open(image).convert('RGB')
+        self.load_favicons()
+    
+        
+    def yuv(self, rgb):
+        r,g,b = [ int(i) for i in rgb ]
+        return r,g,b
+        y = int((0.257 * r) + (0.504 * g) + (0.098 * b) + 16)
+        v = int((0.439 * r) - (0.368 * g) - (0.071 * b) + 128)
+        u = int(-(0.148 * r) - (0.291 * g) + (0.439 * b) + 128)
+
+        
+        return (  ) 
+    def load_favicons(self):
+        self.favicons = []
+        cf = open("cache_file")
+        for l in cf:
+            l = l.strip()
+            file,r,g,b = l.split(",")
+            self.favicons.append(kdtree.P(file, self.yuv((r,g,b))))
+        self.favicons = kdtree.kdtree(self.favicons)
+        
+            
+
+    def mosaic(self):
+        width, height = self.im.size
+        new_im = Image.new(self.im.mode, (height * 16, width * 16))
+        p_data = self.im.load()
+        for x in xrange(0,width):
+            for y in xrange(0,height):
+                nn = kdtree.nearestn(self.yuv(p_data[x,y]), self.favicons)[0].location.data
+                new_im.paste(Image.open(nn), (x * 16 , y * 16))
+
+        new_im.show()
+                
+
+    def pixelate(self,box_size=10):
+        color_avgs = defaultdict(list)
+        new_im = Image.new(self.im.mode, self.im.size)
+
+        width, height = self.im.size
+        p_data = self.im.load()
+        new_p_data = new_im.load()
+
+
+        for x in xrange(0,width):
+            column = x / box_size
+            for y in xrange(0,height):
+                row = y / box_size
+                color_avgs[(row,column)].append(p_data[x,y])
+
+        for k,v in color_avgs.iteritems():
+            l = len(v)
+            r = sum(i[0] for i in v) / l
+            g = sum(i[1] for i in v) / l
+            b = sum(i[2] for i in v) / l
+            color_avgs[k] = (r,g,b) 
+
+        for x in xrange(0,width):
+            column = x / box_size
+            for y in xrange(0,height):
+                row = y / box_size
+                new_p_data[x,y] = color_avgs[row,column]
+
+
+        new_im.show()
+
+
+if __name__ == '__main__':
+    image = ImageOps(sys.argv[1])
+    image.mosaic()
